f8a5946c63a9f7f97ffabde4563d56667e11ef24
siddharth424 <73775321+siddharth424@users.noreply.github.com>
2020-11-29 15:38:10 +0530

    Create mariomore.c

---
+++ task-10/pset-1/mario/mariomore.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void){
+    int height,space,hash;
+
+    do 
+    {
+        printf("Height:");
+        height=GetInt();
+        printf("\n");
+    }
+    while(height<=0||height>=23);
+    for (int i = 1; i <= height; i++)
+    {
+        for (space=(height-i);space>= 0;space--){
+            printf(" ");
+        }
+        for (hash=1;hash<(i+1);hash++){
+            printf("#");    
+        }
+        printf("  ");
+        for (hash=1;hash<(i+1);hash++){
+            printf("#");    
+        }
+        printf("\n");
+    }
+    return 0;
+}
