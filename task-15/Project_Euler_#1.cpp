884491105c70efda218ff72afd1559932b4fe908
siddharth424 <73775321+siddharth424@users.noreply.github.com>
2020-11-30 18:23:03 +0530

    Create Project_Euler_#1.cpp

---
+++ task-15/Project_Euler_#1.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        cin >> n;
+        int temp=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i%3==0 ||i%5==0)
+            {
+                temp=temp+i;
+            }
+        }
+        cout<<temp<<endl;
+    }
+    return 0;
+}
